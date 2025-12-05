9c206188862265be0ddce5635f7d89a416692d48
Adil Azhari <adelazhry1997@gmail.com>
2025-12-05 22:23:01 +0800

    Fix Rector configuration by removing invalid rule

--- rector.php
+++ rector.php
@@ -3,20 +3,33 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Exception\Configuration\InvalidConfigurationException;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
-return RectorConfig::configure()
-    ->withPaths([
-        __DIR__.'/app',
-        __DIR__.'/bootstrap',
-        __DIR__.'/config',
-        __DIR__.'/public',
-        __DIR__.'/resources',
-        __DIR__.'/routes',
-        __DIR__.'/tests',
-    ])
-    // uncomment to reach your current PHP version
-    ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
+try {
+    return RectorConfig::configure()
+        ->withPaths([
+            __DIR__ . '/app',
+            __DIR__ . '/bootstrap',
+            __DIR__ . '/config',
+            __DIR__ . '/public',
+            __DIR__ . '/resources',
+            __DIR__ . '/routes',
+            __DIR__ . '/tests',
+        ])
+        // uncomment to reach your current PHP version
+        ->withPhpSets()
+        ->withAttributesSets()
+        ->withDeadCodeLevel(1)
+        ->withPreparedSets(typeDeclarations: true)
+        ->withCodeQualityLevel(1)
+        ->withConfiguredRule(DisallowedEmptyRuleFixerRector::class, [
+            'treat_as_non_empty' => false,
+        ])
+        ->withRules([
+            DeclareStrictTypesRector::class,
+        ]);
+} catch (InvalidConfigurationException $e) {
 
-    ->withCodeQualityLevel(0);
+}
